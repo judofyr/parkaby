4f0de2248817364eb685a6f8e4101c0918b45de3
Magnus Holm <judofyr@gmail.com>
2009-07-11 16:31:31 +0200

    Add Erector and Nokogiri to the benchmarks

--- bench/run.rb
+++ bench/run.rb
@@ -9,6 +9,9 @@ require 'parkaby'
 require 'erubis'
 require 'tagz'
 require 'haml'
+require 'erector'
+require 'nokogiri'
+include Erector::Mixin
 
 @obj = Object.new
 
@@ -22,11 +25,15 @@ def define_mab(content)
   @cpark = @park.compile
   @park.def_method(@obj, :parkaby)
   eval("def @obj.parkaby_inline; Parkaby { #{content} }; end")
+  eval("def @obj.erector_inline; erector { #{content} }; end")
+  eval("def @obj.nokogiri_inline; Nokogiri::HTML::Builder.new { #{content.gsub('text', 'cdata')} }.to_html; end")
   @mark = Markaby::Template.new(content)
   [['Parkaby (def_method)', '@obj.parkaby'],
    ['Parkaby (render)', '@cpark.render'],
    ['Parkaby (inline)', '@obj.parkaby_inline'],
-   ['Markaby', '@mark.render']]
+   ['Markaby', '@mark.render'],
+   ['Erector', '@obj.erector_inline'],
+   ['Nokogiri', '@obj.nokogiri_inline']]
 end
 
 def define_tagz(content)
@@ -45,6 +52,18 @@ max = ARGV[1].to_i
 bench = Dir["#{dir}/#{name}.*"].inject([]) do |m, f|
   ext = File.extname(f)[1..-1]
   m + send("define_#{ext}", File.read(f))
+end
+
+if max.zero?
+  result = []
+  
+  bench.each do |name, func|
+    result << [name, eval(func)]
+    puts " #{name} ".center(30, "-")
+    puts result[-1][1]
+  end
+  
+  exit
 end
 
 Benchmark.bmbm(20) do |x|
